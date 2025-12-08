25e4be5781dd491c3908032afe220638554eb2e1
RSTrivia <silenthurricane27@gmail.com>
2025-12-08 16:00:48 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,12 +1,12 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-// Replace with your Supabase details
-const SUPABASE_URL = "https://nnlkcwvqhkxasjtshvpw.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ubGtjd3ZxaGt4YXNqdHNodnB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxODY4MzAsImV4cCI6MjA4MDc2MjgzMH0.BtEWYzE4ZA6Fc8rr0n28fPhvIcWdwzoBaOMbAqHYoAo";
+// Read Cloudflare Pages environment variables
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
 
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
 );
+
 
 
