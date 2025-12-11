fa5650e15f33bb5584b8502fe4a97752d5637927
RSTrivia <silenthurricane27@gmail.com>
2025-12-11 16:48:59 +0200

    Create background.js

---
+++ background.js
@@ -0,0 +1,25 @@
+// === BACKGROUND ROTATION ===
+
+// List your backgrounds
+const backgrounds = [
+  "images/background.jpg",
+  "images/background2.jpg",
+  "images/background3.jpg"
+];
+
+// Change every 10 minutes (600000 ms)
+const CHANGE_INTERVAL = 600000;
+
+function changeBackground() {
+  const random = Math.floor(Math.random() * backgrounds.length);
+  document.body.style.backgroundImage = `url('${backgrounds[random]}')`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+}
+
+// First background on page load
+document.addEventListener("DOMContentLoaded", changeBackground);
+
+// Rotate every X minutes
+setInterval(changeBackground, CHANGE_INTERVAL);
