39822a920ea7e866673e247954e85a2ee8ba03cd
RSTrivia <silenthurricane27@gmail.com>
2025-12-20 15:18:27 +0200

    Update bgWorker.js

--- bgWorker.js
+++ bgWorker.js
@@ -1,20 +1,3 @@
-/*let currentBg;
-let backgrounds;
-const CHANGE_INTERVAL = 18000;
-
-onmessage = (e) => {
-  currentBg = e.data.current;
-  backgrounds = e.data.backgrounds;
-
-  setInterval(() => {
-    // pick a new background different from current
-    const choices = backgrounds.filter(b => b !== currentBg);
-    const nextBg = choices[Math.floor(Math.random() * choices.length)];
-
-    currentBg = nextBg;
-    postMessage(nextBg);
-  }, CHANGE_INTERVAL);*/
-/*};*/
 let currentBg;
 let backgrounds;
 const CHANGE_INTERVAL = 240000; // 4 minutes
