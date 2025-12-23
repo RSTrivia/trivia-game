edc24a1d86770b6f6604aead1632f5daebef37ea
RSTrivia <silenthurricane27@gmail.com>
2025-12-23 23:17:00 +0200

    Refactor config.js to use CONFIG object

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-window.CONFIG_SUPABASE_URL = "https://nnlkcwvqhkxasjtshvpw.supabase.co";
-window.CONFIG_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ubGtjd3ZxaGt4YXNqdHNodnB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxODY4MzAsImV4cCI6MjA4MDc2MjgzMH0.BtEWYzE4ZA6Fc8rr0n28fPhvIcWdwzoBaOMbAqHYoAo";
-
+const CONFIG = {
+    // This is public and safe to share!
+    BRIDGE_URL: 'https://supabase-bridge-zzqp.onrender.com/api'
+};
