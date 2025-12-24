af4537ea950fae80977839f9806f2d869493371a
RSTrivia <silenthurricane27@gmail.com>
2025-12-24 02:18:27 +0200

    Update bgWorker.js

--- bgWorker.js
+++ bgWorker.js
@@ -3,15 +3,16 @@ let backgrounds;
 const CHANGE_INTERVAL = 20000;//240000; 
 
 onmessage = (e) => {
-  currentBg = e.data.current;
-  backgrounds = e.data.backgrounds;
+    currentBg = e.data.current;
+    backgrounds = e.data.backgrounds;
 
-  // This ensures the background won't change for at least 4 mins after a page load/refresh
-  setInterval(() => {
-    const choices = backgrounds.filter(b => b !== currentBg);
-    const nextBg = choices[Math.floor(Math.random() * choices.length)];
+    // By using setInterval without an immediate call, 
+    // nothing happens for the first 4 minutes.
+    setInterval(() => {
+        const choices = backgrounds.filter(b => b !== currentBg);
+        const nextBg = choices[Math.floor(Math.random() * choices.length)];
 
-    currentBg = nextBg;
-    postMessage(nextBg);
-  }, CHANGE_INTERVAL);
+        currentBg = nextBg;
+        postMessage(nextBg);
+    }, CHANGE_INTERVAL);
 };
