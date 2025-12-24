d668e8c759ef985b1c3055812730c0411c9342ae
RSTrivia <silenthurricane27@gmail.com>
2025-12-24 12:05:57 +0200

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-window.CONFIG_SUPABASE_URL = "https://nnlkcwvqhkxasjtshvpw.supabase.co";
-window.CONFIG_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ubGtjd3ZxaGt4YXNqdHNodnB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxODY4MzAsImV4cCI6MjA4MDc2MjgzMH0.BtEWYzE4ZA6Fc8rr0n28fPhvIcWdwzoBaOMbAqHYoAo";
-
