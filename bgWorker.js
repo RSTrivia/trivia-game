3433aa0340988d03814459d256217b07c14e6903
RSTrivia <silenthurricane27@gmail.com>
2025-12-24 02:21:49 +0200

    Update bgWorker.js

--- bgWorker.js
+++ bgWorker.js
@@ -1,18 +1,17 @@
 let currentBg;
 let backgrounds;
-const CHANGE_INTERVAL = 20000;//240000; 
+const CHANGE_INTERVAL = 50000;//240000; // 4 minutes
 
 onmessage = (e) => {
-    currentBg = e.data.current;
-    backgrounds = e.data.backgrounds;
+  currentBg = e.data.current;
+  backgrounds = e.data.backgrounds;
 
-    // By using setInterval without an immediate call, 
-    // nothing happens for the first 4 minutes.
-    setInterval(() => {
-        const choices = backgrounds.filter(b => b !== currentBg);
-        const nextBg = choices[Math.floor(Math.random() * choices.length)];
+  setInterval(() => {
+    // pick a new background different from current
+    const choices = backgrounds.filter(b => b !== currentBg);
+    const nextBg = choices[Math.floor(Math.random() * choices.length)];
 
-        currentBg = nextBg;
-        postMessage(nextBg);
-    }, CHANGE_INTERVAL);
+    currentBg = nextBg;
+    postMessage(nextBg);
+  }, CHANGE_INTERVAL);
 };
